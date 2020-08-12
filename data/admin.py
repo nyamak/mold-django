9b5f493aef8941d51741a2b993efc373b76c0a6f
Nicolas Yamakoshi <nicolas.yamakoshi@gmail.com>
2020-08-11 21:17:05 -0300

    refactor: Add payload to admin view.

--- data/admin.py
+++ data/admin.py
@@ -15,5 +15,5 @@ class DeviceAdmin(admin.ModelAdmin):
 
 @admin.register(models.Measurement)
 class MeasurementAdmin(admin.ModelAdmin):
-    list_display = ('device', 'temperature', 'humidity', 'date', )
+    list_display = ('device', 'temperature', 'humidity', 'date', 'payload',)
     list_filter = ('device', )
